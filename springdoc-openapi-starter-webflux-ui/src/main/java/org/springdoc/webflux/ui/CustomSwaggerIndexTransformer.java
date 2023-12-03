fe309caf13934caa442e96c9b3e7c2a2b14973cf
ddark008 <i@ddark008.ru>
2023-12-03 20:49:21 +0300

    docs: add javadocs for CustomSwaggerIndexTransformer

--- springdoc-openapi-starter-webflux-ui/src/main/java/org/springdoc/webflux/ui/CustomSwaggerIndexTransformer.java
+++ springdoc-openapi-starter-webflux-ui/src/main/java/org/springdoc/webflux/ui/CustomSwaggerIndexTransformer.java
@@ -1,10 +1,20 @@
 package org.springdoc.webflux.ui;
 
+import org.springdoc.ui.AbstractSwaggerIndexTransformer;
 import org.springframework.web.server.ServerWebExchange;
 
 /**
- *
+ * The interface for custom Swagger index page transformer.
+ * <p>
+ * All beans with this interface apply after {@link AbstractSwaggerIndexTransformer#defaultTransformations}
+ * @author ddark008
  */
 public interface CustomSwaggerIndexTransformer {
-    String transform(ServerWebExchange serverWebExchange, String html);
+    /**
+     * Custom transform for the Swagger index page
+     * @param exchange the current exchange
+     * @param html the Swagger index page to transform
+     * @return the transformed Swagger index page
+     */
+    String transform(ServerWebExchange exchange, String html);
 }
--- springdoc-openapi-starter-webmvc-ui/src/main/java/org/springdoc/webmvc/ui/CustomSwaggerIndexTransformer.java
+++ springdoc-openapi-starter-webmvc-ui/src/main/java/org/springdoc/webmvc/ui/CustomSwaggerIndexTransformer.java
@@ -1,7 +1,20 @@
 package org.springdoc.webmvc.ui;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springdoc.ui.AbstractSwaggerIndexTransformer;
 
+/**
+ * The interface for custom Swagger index page transformer.
+ * <p>
+ * All beans with this interface apply after {@link AbstractSwaggerIndexTransformer#defaultTransformations}
+ * @author ddark008
+ */
 public interface CustomSwaggerIndexTransformer {
+    /**
+     * Custom transform for the Swagger index page
+     * @param request the current request
+     * @param html the Swagger index page to transform
+     * @return the transformed Swagger index page
+     */
     String transform(HttpServletRequest request, String html);
 }
